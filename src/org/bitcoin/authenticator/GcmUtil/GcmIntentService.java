4840bfddd653ef0fa20aba246e12d0400cf4f419
Alon Muroch <alonmuroch@gmail.com>
2014-05-31 09:56:39 +0200

    pending request update

--- src/org/bitcoin/authenticator/GcmUtil/GcmIntentService.java
+++ src/org/bitcoin/authenticator/GcmUtil/GcmIntentService.java
@@ -42,43 +42,95 @@ public class GcmIntentService extends IntentService {
         GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
         // The getMessageType() intent parameter must be the intent you received
         // in your BroadcastReceiver.
-        String messageType = gcm.getMessageType(intent);
-
-        if (!extras.isEmpty()) {  // has effect of unparcelling Bundle
-            /*
-             * Filter messages based on message type. Since it is likely that GCM
-             * will be extended in the future with new message types, just ignore
-             * any message types you're not interested in, or that you don't
-             * recognize.
-             */
-            if (GoogleCloudMessaging.
-                    MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
-                sendNotification("Send error: " + extras.toString());
-            } else if (GoogleCloudMessaging.
-                    MESSAGE_TYPE_DELETED.equals(messageType)) {
-                sendNotification("Deleted messages on server: " +
-                        extras.toString());
-            // If it's a regular GCM message, do some work.
-            } else if (GoogleCloudMessaging.
-                    MESSAGE_TYPE_MESSAGE.equals(messageType)) {
-                alertUserOnNotification(extras);
-                Log.v(GcmUtilGlobal.TAG, "Received: " + extras.getString("data"));
-            }
-        }
+        try {
+		    if (!extras.isEmpty()) { 
+		    	RequestType messageType = getRequestType(extras.getString("data"));
+				
+		        if (messageType == RequestType.signTx) {
+		        	processNewSigningNotification(extras.getString("data"));
+		            Log.v(GcmUtilGlobal.TAG, "Received: " + extras.getString("data"));
+		        }
+		        else if(messageType == RequestType.updateIpAddressesForPreviousMessage){
+		        	processUpdateIpAddressesForPreviousMessage(extras.getString("data"));
+		        	Log.v(GcmUtilGlobal.TAG, "Received: " + extras.getString("data"));
+		        }
+		    }
+        } catch (JSONException e) { e.printStackTrace(); }
+        
         // Release the wake lock provided by the WakefulBroadcastReceiver.
         GcmBroadcastReceiver.completeWakefulIntent(intent);
     }
+    
+    public RequestType getRequestType(String msg) throws JSONException{
+    	obj = new JSONObject(msg);
+    	int objInt = obj.getInt("RequestType");
+    	switch(objInt){
+    	case 1:
+    		return RequestType.test;
+    	case 2:
+    		return RequestType.signTx;
+    	case 4:
+    		return RequestType.updateIpAddressesForPreviousMessage;
+    	}
+    	return null;
+    }
 
-    private void alertUserOnNotification(Bundle extras)
-    {
-    	sendNotification(extras.getString("data"));
-    	// Example - Bundle[{message=Test bulk notification, android.support.content.wakelockid=1, collapse_key=message, from=204673947609}]
+    private void processUpdateIpAddressesForPreviousMessage(String msg) throws JSONException {
+    	obj = new JSONObject(msg);
+    	JSONObject payload = new JSONObject(obj.getString("ReqPayload"));
+    	// search notification
+    	SharedPreferences settings = getSharedPreferences("ConfigFile", 0);
+		SharedPreferences.Editor editor = settings.edit();	
+		// update RequestID list
+		JSONArray o;
+		if(settings.getString("pendingList", null) !=null){
+			o = new JSONArray(settings.getString("pendingList", ""));
+			 for(int i = 0 ; i < o.length(); i++){
+				String pendingID = o.get(i).toString();
+				if(pendingID.equals(obj.getString("RequestID"))){
+					JSONObject pendingObj = new JSONObject(settings.getString(pendingID, null));
+					// update
+					JSONObject pendingPayload = new JSONObject(pendingObj.getString("ReqPayload"));
+					pendingPayload.put("ExternalIP", payload.getString("ExternalIP"));
+					pendingPayload.put("LocalIP", payload.getString("LocalIP"));
+					pendingObj.put("ReqPayload",pendingPayload );
+					editor.putString(pendingObj.getString("RequestID"), pendingObj.toString());
+					editor.commit();
+					//
+					break;
+				}
+			 }
+		}
+		
+		Intent intent = new Intent(this, Main.class);
+		intent.putExtra("RequestID", obj.getString("RequestID"));
+		intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                intent, PendingIntent.FLAG_UPDATE_CURRENT);
+		
+		mNotificationManager = (NotificationManager)
+                this.getSystemService(Context.NOTIFICATION_SERVICE);
+		//
+		String customMsg = "Pending Notification Update";
+		NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+        .setSmallIcon(R.drawable.authenticator_logo)
+        .setContentTitle("New Message To Sign")
+        .setStyle(new NotificationCompat.BigTextStyle()
+        .bigText(customMsg))
+        .setContentText(customMsg)
+        .setDefaults(Notification.DEFAULT_SOUND)
+        .setDefaults(Notification.DEFAULT_VIBRATE)
+        .setTicker(customMsg).setWhen(System.currentTimeMillis());
+
+		mBuilder.setContentIntent(contentIntent);
+        Notification notif = mBuilder.build();
+        notif.flags |= Notification.FLAG_AUTO_CANCEL;
+        mNotificationManager.notify((int)uniqueId, notif);
     }
     
-    // Put the message into a notification and post it.
-    // This is just one simple example of what you might choose to do with
-    // a GCM message.
-    private void sendNotification(String msg) {
+    private void processNewSigningNotification(String msg) throws JSONException {
     	Date now = new Date();
     	uniqueId = now.getTime();//use date to generate an unique id to differentiate the notifications.
     	
@@ -87,24 +139,23 @@ public class GcmIntentService extends IntentService {
 
     	Intent intent = new Intent(this, Main.class);
     	String customMsg = "";
-		try {
-			obj = new JSONObject(msg);
-			// Those flags would serve to create a list of pending requests
-			obj.put("seen", false);
-			/**
-			 * When a new notification enters we have 2 options:
-			 * 1) If the app is not running, put the request ID as an extra so it will pull it on launch.
-			 * 2) If app is already running we add it to a request queue so {@link org.bitcoin.authenticator.Wallet_list}<br>
-			 * 	  activity will pull it in the while loop 
-			 */
-			// 1) 
-			intent.putExtra("RequestID", obj.getString("RequestID"));
-			intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-			// 2) 
-			addRequestToQueue(obj.getString("RequestID"));
-			
-			customMsg = obj.getString("CustomMsg");
-		} catch (JSONException e) {e.printStackTrace();}
+	
+		obj = new JSONObject(msg);
+		// Those flags would serve to create a list of pending requests
+		obj.put("seen", false);
+		/**
+		 * When a new notification enters we have 2 options:
+		 * 1) If the app is not running, put the request ID as an extra so it will pull it on launch.
+		 * 2) If app is already running we add it to a request queue so {@link org.bitcoin.authenticator.Wallet_list}<br>
+		 * 	  activity will pull it in the while loop 
+		 */
+		// 1) 
+		intent.putExtra("RequestID", obj.getString("RequestID"));
+		intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		// 2) 
+		addRequestToQueue(obj.getString("RequestID"));
+		
+		customMsg = obj.getString("CustomMsg");
   
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
                 intent, PendingIntent.FLAG_UPDATE_CURRENT);
@@ -124,23 +175,29 @@ public class GcmIntentService extends IntentService {
         Notification notif = mBuilder.build();
         notif.flags |= Notification.FLAG_AUTO_CANCEL;
         mNotificationManager.notify((int)uniqueId, notif);
-    	try {
-    		SharedPreferences settings = getSharedPreferences("ConfigFile", 0);
-    		SharedPreferences.Editor editor = settings.edit();	
-        	editor.putBoolean("request", true);
-			editor.putString(obj.getString("RequestID"), obj.toString());
-			// update RequestID list
-			JSONArray o;
-			if(settings.getString("pendingList", null) !=null)
-				o = new JSONArray(settings.getString("pendingList", ""));
-			else
-				o = new JSONArray();
-			o.put(obj.getString("RequestID"));
-			editor.putString("pendingList", o.toString());
-			editor.commit();
-		} catch (JSONException e) { e.printStackTrace(); }
+
+        // update preference
+		SharedPreferences settings = getSharedPreferences("ConfigFile", 0);
+		SharedPreferences.Editor editor = settings.edit();	
+    	editor.putBoolean("request", true);
+		editor.putString(obj.getString("RequestID"), obj.toString());
+		// update RequestID list
+		JSONArray o;
+		if(settings.getString("pendingList", null) !=null)
+			o = new JSONArray(settings.getString("pendingList", ""));
+		else
+			o = new JSONArray();
+		o.put(obj.getString("RequestID"));
+		editor.putString("pendingList", o.toString());
+		editor.commit();
     	
     }
+        
+    //########################
+    //
+    //			QUEUE
+    //
+    //########################
     
     private static BlockingQueue<String> getQueue(){
     	if(requestQueue == null){
--- src/org/bitcoin/authenticator/GcmUtil/RequestType.java
+++ src/org/bitcoin/authenticator/GcmUtil/RequestType.java
@@ -2,7 +2,8 @@ package org.bitcoin.authenticator.GcmUtil;
 
 public enum RequestType {
 	test (1 << 0), // 1
-	signTx (1 << 1) // 2
+	signTx (1 << 1), // 2
+	updateIpAddressesForPreviousMessage ( 1 << 2 ) // 4
 	;
 	;
 	private final int id;
