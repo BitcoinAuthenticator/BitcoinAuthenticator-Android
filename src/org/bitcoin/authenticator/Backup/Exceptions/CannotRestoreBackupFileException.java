3a4233d3c784f771611e73f6fa426b89e85ac2b6
alon muroch <alonmuroch@gmail.com>
2014-12-06 19:34:06 +0200

    first commit android studio gradle build

--- src/org/bitcoin/authenticator/Backup/Exceptions/CannotRestoreBackupFileException.java
+++
@@ -1,7 +0,0 @@
-package org.bitcoin.authenticator.Backup.Exceptions;
-
-public class CannotRestoreBackupFileException extends Exception {
-	public CannotRestoreBackupFileException(String msg) {
-		super(msg);
-	}
-}
--- src/org/bitcoin/authenticator/core/exceptions/NoSeedOrMnemonicsFound.java
+++
@@ -1,7 +0,0 @@
-package org.bitcoin.authenticator.core.exceptions;
-
-public class NoSeedOrMnemonicsFound extends Exception{
-	public NoSeedOrMnemonicsFound(String msg){
-		super(msg);
-	}
-}
